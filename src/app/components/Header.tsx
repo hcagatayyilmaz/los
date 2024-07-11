417884b734198cdaa60b010d152ce87e65d95c38
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-07-11 20:17:28 +0200

    being bad at designin

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -15,28 +15,28 @@ interface LogoProps {
 export default function Header({name, user}: LogoProps) {
     return (
         <div
-            className={`px-2 mt-2 pb-4 flex justify-between border-b-2 border-black bg-transparent`}
+            className={`px-2 mt-2 pb-4 flex items-center justify-center border-b-2 border-black bg-transparent`}
         >
             <Link href={"/"}>
                 <div
-                    className={`flex items-center bg-white px-4 py-2 rounded-3xl space-x-2 ${museumModerno.className}`}
+                    className={`flex items-end bg-white px-4 py-1 rounded-3xl space-x-2 ${museumModerno.className}`}
                 >
-                    <h1 className='text-4xl font-semibold'>
+                    <h1 className='text-3xl font-medium'>
                         <span className='text-black '>Los</span>
                         <span className='text-customYellow text-4xl'>.</span>
                     </h1>
-                    <div className='flex items-center justify-center'>
-                        <h2 className='text-lg font-semibold text-customYellow  top-3 -left-4'>
+                    <div className='flex items-end justify-end'>
+                        <h2 className='text-md font-semibold text-customYellow  top-3 -left-4'>
                             {name}
                         </h2>
                     </div>
                 </div>
             </Link>
 
-            <div className='flex items-center justify-center bg-pink-100 text-customYellow rounded-full px-4'>
+            {/* <div className='flex items-center justify-center bg-pink-100 text-customYellow rounded-full px-4'>
                 <CoinIcon className={"text-customYellow"} />
                 <span className='text-sm'>+40</span>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/app/components/ItemCard.tsx
+++ src/app/components/ItemCard.tsx
@@ -3,30 +3,32 @@ import Image from "next/image"
 
 export const ItemCard = () => {
     return (
-        <div className='absolute bottom-2 left-2 right-2 bg-white rounded-3xl shadow-lg p-4 flex items-start space-x-4 mx-2 border-8 border-customYellow'>
+        <div className='absolute bottom-2 h-48 left-2 right-2 bg-white rounded-2xl shadow-lg p-2 flex gap-2 items-start justify-between mx-1 border-4 border-customYellow'>
             <div className='w-1/3 h-full relative'>
                 <Image
-                    src='/tuebingen.jpg' // Path to the image in the public directory
+                    src='/tuebingen.jpg' // Ensure this path is correct and the image is in the public directory
                     alt='Markplatz'
                     layout='fill'
                     objectFit='cover'
-                    className='rounded-lg'
+                    className='rounded-2xl'
                 />
             </div>
-            <div className='w-2/3'>
-                <div className='flex justify-between items-center'>
-                    <h2 className='text-2xl font-bold'>Markplatz</h2>
+            <div className='w-2/3 flex flex-col justify-between h-full'>
+                <div>
+                    <div className='flex justify-between items-center'>
+                        <h2 className='text-lg font-bold'>Markplatz</h2>
+                    </div>
+                    <p className='text-gray-600 text-sm'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, felis
+                        consequat.
+                    </p>
                 </div>
-                <p className='text-gray-600 text-sm'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, felis
-                    consequat.
-                </p>
-                <div className='flex items-center mt-4'>
+                <div className='flex items-center'>
                     <div className='flex items-center bg-pink-100 text-pink-500 rounded-full px-4 py-2 mr-4'>
                         <CoinIcon className='text-pink-500' />
-                        <span className='ml-2 text-md'>100</span>
+                        <span className='ml-2 text-sm'>100</span>
                     </div>
-                    <button className='bg-green-800 text-white rounded-full px-4 py-2'>
+                    <button className='bg-green-800 text-white text-sm rounded-full px-4 py-2'>
                         Check In
                     </button>
                 </div>
