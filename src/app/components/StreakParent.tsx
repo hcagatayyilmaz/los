4adae2076598da5990323c4ee8a8b7821bf92691
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-07-28 15:13:03 +0200

    why

--- src/app/components/StreakParent.tsx
+++ src/app/components/StreakParent.tsx
@@ -10,7 +10,7 @@ function StreakParent() {
     return (
         <div className='w-full flex flex-col gap-4'>
             <h1 className={`px-6 mt-2 text-xl font-bold ${museoModerno.className}`}>
-                Tübingen's Nature
+                Tübingen&#x27;s Nature
             </h1>
             <p className={`px-6 ${museoModerno.className} text-sm`}>
                 Visit places with tree icon on the map to get rewards!
