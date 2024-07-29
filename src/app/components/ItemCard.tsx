4aec629f615e6724f6793239e4cc04b903c5ba32
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-07-29 15:28:33 +0200

    a bit more

--- src/app/components/ItemCard.tsx
+++ src/app/components/ItemCard.tsx
@@ -11,23 +11,23 @@ export const ItemCard = ({location}: ItemCardProps) => {
     return (
         <div className='w-full max-w-md bg-white rounded-xl shadow-xl p-4 flex flex-col border border-gray-300'>
             <div className='flex'>
-                <div className='w-1/3 h-24 relative'>
-                    {location.imageUrl && (
+                {location.imageUrl && (
+                    <div className='w-1/3 h-24 relative'>
                         <Image
                             src={location.imageUrl}
                             fill
-                            style={{objectFit: "cover"}} // Updated
+                            style={{objectFit: "cover"}}
                             className='rounded-lg'
-                            alt='Tübingen, Germany'
+                            alt='Location Image'
                         />
-                    )}
-                </div>
-                <div className='w-2/3 pl-4'>
+                    </div>
+                )}
+                <div className={`pl-4 ${location.imageUrl ? "w-2/3" : "w-full"}`}>
                     <h2 className='text-md font-bold text-wrap'>{location.name_en}</h2>
                     {/* <div className='flex items-center text-pink-500 my-2'>
-                        <CoinIcon className='w-5 h-5 mr-1' />
-                        <span className='font-semibold'>{location.points}</span>
-                    </div> */}
+            <CoinIcon className='w-5 h-5 mr-1' />
+            <span className='font-semibold'>{location.points}</span>
+        </div> */}
                     <p className='text-gray-500 text-xs text-wrap'>{location.description_en}</p>
                 </div>
             </div>
