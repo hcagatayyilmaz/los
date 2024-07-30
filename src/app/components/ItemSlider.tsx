1923760e34d55d27997545f6b1b3273fc282ca20
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-07-30 17:06:03 +0200

    skinny fat

--- src/app/components/ItemSlider.tsx
+++ src/app/components/ItemSlider.tsx
@@ -11,41 +11,13 @@ type ItemsSliderProps = {
 const ItemsSlider = ({locations}: ItemsSliderProps) => {
     const sliderRef = useRef<HTMLDivElement>(null)
     const itemRefs = useRef<Record<string, HTMLDivElement | null>>({})
-    const {selectedLocation, setSelectedLocation} = useSelectedItem()
+    const {selectedLocation} = useSelectedItem()
 
     useEffect(() => {
         if (selectedLocation && itemRefs.current[selectedLocation.id]) {
             itemRefs.current[selectedLocation.id]?.scrollIntoView({behavior: "smooth"})
         }
     }, [selectedLocation])
-
-    useEffect(() => {
-        const slider = sliderRef.current
-        if (!slider) return
-
-        const handleScroll = () => {
-            const center = slider.scrollLeft + slider.clientWidth / 2
-            let closestLocation = locations[0]
-            let closestDistance = Math.abs(
-                (itemRefs.current[locations[0].id]?.offsetLeft ?? 0) - center || 0
-            )
-
-            locations.forEach((location) => {
-                const itemRef = itemRefs.current[location.id]
-                if (!itemRef) return
-                const distance = Math.abs(itemRef.offsetLeft + itemRef.clientWidth / 2 - center)
-                if (distance < closestDistance) {
-                    closestLocation = location
-                    closestDistance = distance
-                }
-            })
-
-            setSelectedLocation(closestLocation)
-        }
-
-        slider.addEventListener("scroll", handleScroll)
-        return () => slider.removeEventListener("scroll", handleScroll)
-    }, [locations, setSelectedLocation])
 
     return (
         <div ref={sliderRef} className='overflow-x-scroll whitespace-nowrap bottom-0 left-0'>
