a0d3e91144bf4e2f25eb7e57627a3660ab154886
hcagatayyilmaz <h.cagatayyilmaz@gmail.com>
2024-07-30 15:32:21 +0200

    distance

--- src/app/server/index.ts
+++ src/app/server/index.ts
@@ -33,7 +33,7 @@ export async function checkIn({
     }
 
     const distance = await calculateDistance3(userLat, userLng, place.latitude, place.longitude)
-    if (distance <= 25) {
+    if (distance <= 20) {
         // 25 meters threshold
         const checkIn = await prisma.checkIn.create({
             data: {
